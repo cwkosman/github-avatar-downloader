f0b620f7a0c8598090f49300c416d951c574eec1
cwkosman <cwkosman@gmail.com>
2017-02-14 01:10:27 +0000

    Define callback, modify request options.
    Request options now takes a parameter {headers: 'User-Agent': 'cwkosman'} per Github's requirements.
    
    Caallback function now expressed, rather than defined anonymously on calling getRepoContributors.

--- download_avatars.js
+++ download_avatars.js
@@ -1,15 +1,25 @@
 var request = require('request');
+
 var GITHUB_USER = 'cwkosman';
 var GITHUB_TOKEN = 'c29cfb18893c4c4b647d69cba86ce90b6975bdcc';
 
 console.log('Welcome to the Github Avatar Downloader!');
 
-function getRepoContributors(repoOwner, repoName, cb) {
-  var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  console.log(requestURL);
+function callback (err, response, body) {
+  if (err) { throw err; }
+  console.log(response.statusCode);
+  console.log(body);
 }
 
-getRepoContributors("jquery", "jquery", function (err, result) {
-  console.log("Errors: ", err);
-  console.log("Result: ", result);
-});
+function getRepoContributors(repoOwner, repoName, cb) {
+  var requestOptions = {
+    url: 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+    headers: {
+      'User-Agent': 'cwkosman'
+    }
+  };
+  request(requestOptions, cb);
+  console.log(cb);
+}
+
+getRepoContributors("jquery", "jquery", callback);
