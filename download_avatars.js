75eabc3fb4a10e12fa975b2198d05c8824fff1b0
cwkosman <cwkosman@gmail.com>
2017-02-14 03:49:30 +0000

    Check user input and status code
    Add check that user has specified arguments for both repoOwner and repoName.
    If not, notify user to specify both.
    Callback checks if no errors and statusCode === 200 before proceeding.

--- download_avatars.js
+++ download_avatars.js
@@ -18,21 +18,28 @@ function downloadImageByURL(url, filePath) {
 }
 
 function callback (err, response, body) {
-  if (err) { throw err; }
-  var contributors = JSON.parse(body);
-  //TODO : Loop over all pages of reponse - by default GitHub returns 30 per page
-  //var pageArray = response.headers.link.split(',');
-  //var pageNumberMatch = /page=(.*?)>/g;
-  //var pageNumber = pageNumberMatch.exec(pageArray[1])[1];
-  //console.log(pageNumber);
-  contributors.forEach((contributor) => {
-    //TODO - Get file extension of avatar as saved on Github, rather than appending .jpg
-    var filePath = `./avatars/${contributor.login}.jpg`;
-    downloadImageByURL(contributor.avatar_url, filePath);
-  });
+  if (!err && response.statusCode === 200) {
+    var contributors = JSON.parse(body);
+    //TODO : Loop over all pages of reponse - by default GitHub returns 30 per page
+    //var pageArray = response.headers.link.split(',');
+    //var pageNumberMatch = /page=(.*?)>/g;
+    //var pageNumber = pageNumberMatch.exec(pageArray[1])[1];
+    //console.log(pageNumber);
+    contributors.forEach((contributor) => {
+      //TODO - Get file extension of avatar as saved on Github, rather than appending .jpg
+      var filePath = `./avatars/${contributor.login}.jpg`;
+      downloadImageByURL(contributor.avatar_url, filePath);
+    });
+  } else if (err) {
+    throw (err);
+  }
 }
 
 function getRepoContributors(repoOwner, repoName, cb) {
+  if (!repoOwner || !repoName) {
+    console.log("Input both a repository owner and repository name, e.g., node download_avatars.js jquery jquery");
+    return;
+  }
   var requestOptions = {
     url: 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
     headers: {
