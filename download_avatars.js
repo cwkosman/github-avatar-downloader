e8e3ff7c5774a0121cae64b94115a66778e4ec74
cwkosman <cwkosman@gmail.com>
2017-02-14 00:24:21 +0000

    Define Github API request URL
    Set up variables defining github user, github access token,
    and full contributors request URL according to Github API docs.

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,12 @@
 var request = require('request');
+var GITHUB_USER = 'cwkosman';
+var GITHUB_TOKEN = 'c29cfb18893c4c4b647d69cba86ce90b6975bdcc';
 
 console.log('Welcome to the Github Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-
+  var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
 }
 
 getRepoContributors("jquery", "jquery", function (err, result) {
